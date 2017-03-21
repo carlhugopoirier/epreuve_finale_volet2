1d5d647e0430bc294383ea25f1359087b76662ee
Carl ye NiceEnTa <poiriercarlhugo@gmail.com>
2017-03-21 15:25:30 -0400

    Étape 1 terminée

--- serveur.js
+++ serveur.js
@@ -3,14 +3,22 @@ var fs = require("fs");
 var obj;
 fs.readFile('public/text/collection_provinces.json', 'utf-8', function (err, data) {
 if (err) return console.error(err);
-
+obj = JSON.stringify(data)
 });
 
+var express = require('express');
+var app = express();
 
+app.get('/', function (req, res) {
+  res.writeHead(200, {"Content-Type": "application/json"});
+  res.write(obj);
+  res.end();
+})
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write(data.toString());
-  response.end();
-}).listen(8081);
-console.log('Server running.');
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+   console.log('Server running.');
+})
