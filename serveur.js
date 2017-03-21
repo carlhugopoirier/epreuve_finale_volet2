6cb3f84fcdc8ee62d666026163519d089ea95f34
Carl ye NiceEnTa <poiriercarlhugo@gmail.com>
2017-03-21 15:46:49 -0400

    Début de l'étape 2

--- serveur.js
+++ serveur.js
@@ -1,19 +1,18 @@
 var http = require("http");
+var express = require('express');
+var app = express();
 var fs = require("fs");
 var obj;
 fs.readFile('public/text/collection_provinces.json', 'utf-8', function (err, data) {
-if (err) return console.error(err);
-obj = JSON.stringify(data)
+		if (err) return console.error(err);
+		obj = JSON.stringify(data)
 });
 
-var express = require('express');
-var app = express();
-
 app.get('/', function (req, res) {
-  res.writeHead(200, {"Content-Type": "application/json"});
-  res.write(obj);
-  res.end();
-})
+		  res.writeHead(200, {"Content-Type": "application/json"});
+		  res.write(obj);
+		  res.end();
+		})
 
 var server = app.listen(8081, function () {
    var host = server.address().address
